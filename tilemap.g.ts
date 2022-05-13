65b75956ff5654ef15286c89e0229931bc36ad6f
Hoang Thien <50955701+hoangthien2003@users.noreply.github.com>
2022-05-13 16:04:50 +0700

    update demo tilemap

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -46,24 +46,24 @@ namespace myTiles {
 . . . . . . . . . . . . . . . . 
 `, [myTiles.transparency16], TileScale.Sixteen);
             case "map-demo":
-            case "level1":return tiles.createTilemap(hex`100010000c0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b100201010d02010201010201010b070707010504030504040505030d03030c070702040809090a050404030f03030d0707010405040303030404030f030d01070702040305030503030605040304010707020303040304050504030303050107070d030304050403030d030303050207070d040504060405050303040404011007010303030304050f0303050404011007020e03060405030f0d03040503020707020e03050303030d030504030301070701030306050303030305030303020707070203030504040505040e0e01070707070701010d020102010102010707070b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b`, img`
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-`, [myTiles.transparency16,sprites.castle.tileGrass2,sprites.castle.tileGrass1,sprites.builtin.forestTiles0,sprites.castle.tileDarkGrass3,sprites.castle.tileDarkGrass2,sprites.castle.tileDarkGrass1,sprites.swamp.swampTile1,sprites.builtin.forestTiles21,sprites.builtin.forestTiles23,sprites.builtin.forestTiles20,sprites.swamp.swampTile0,sprites.swamp.swampTile2,sprites.castle.saplingPine,sprites.castle.shrub,sprites.castle.rock0,sprites.castle.rock2], TileScale.Sixteen);
+            case "level1":return tiles.createTilemap(hex`100010000c0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b100201010d02010201010202010b070707010504030504040505030d03030c070702040809090a050404030f03030d0707010405040303030404030f030d01070702040305030503030605040304010707020303040304050504030303050107070d030304050403030d030303050212070d040504060405050303040404011107010303030304050f0303050404011107020e03060405030f0d03040503021207020e03050303030d030504030301070701030306050303030305030303020707070203030504040505040e0e0d070707070701010d020102010102010707070b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b`, img`
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+. . . . 2 2 2 2 2 2 2 2 2 2 2 2 
+2 . . . 2 . . . . . 2 2 2 2 2 2 
+2 . . 2 2 2 2 . . . 2 2 2 2 2 2 
+2 . . . . 2 2 2 . . 2 2 2 2 . 2 
+2 . . 2 . 2 . 2 2 . . . 2 . . 2 
+2 . 2 2 . 2 . . . . 2 2 2 . . 2 
+2 2 2 2 . . . 2 2 2 2 2 2 . . 2 
+2 2 . . . . . . . 2 2 . . . . . 
+2 2 2 2 2 2 . . 2 2 2 . . . . . 
+2 2 2 2 . . . 2 2 2 2 . . 2 . 2 
+2 2 2 2 . 2 2 2 2 2 . . 2 2 . 2 
+2 2 2 2 . . 2 2 2 2 . 2 2 2 . 2 
+2 2 2 2 2 . . . . . . . . 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+`, [myTiles.transparency16,sprites.castle.tileGrass2,sprites.castle.tileGrass1,sprites.builtin.forestTiles0,sprites.castle.tileDarkGrass3,sprites.castle.tileDarkGrass2,sprites.castle.tileDarkGrass1,sprites.swamp.swampTile1,sprites.builtin.forestTiles21,sprites.builtin.forestTiles23,sprites.builtin.forestTiles20,sprites.swamp.swampTile0,sprites.swamp.swampTile2,sprites.castle.saplingPine,sprites.castle.shrub,sprites.castle.rock0,sprites.castle.rock2,sprites.dungeon.stairLarge,sprites.dungeon.purpleOuterWest2], TileScale.Sixteen);
         }
         return null;
     })
