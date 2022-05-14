e605914d7e96fca7d133e9dfc38c9ae4505b75d5
voluyen <66213157+voluyen@users.noreply.github.com>
2022-05-15 00:03:46 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,65 +1,38 @@
 namespace SpriteKind {
     export const ground = SpriteKind.create()
 }
-function initGround () {
-    ground1 = sprites.create(img`
-        9999999999999999999999999999999999999999999999999999999999999999
-        9999999999999999999999999999999999999999999999999999999999999999
-        9999999999999999999999999999999999999999999999999999999999999999
-        9999999999999999999999999999999999999999999999999999999999999999
-        9999999999999999999999999999999999999999999999999999999999999999
-        9999999999999999999999999999999999999999999999999999999999999999
-        9999999999999999999999999999999999999999999999999999999999999999
-        9999999999999999999999999999999999999999999999999999999999999999
-        9999999999999999999999999999999999999999999999999999999999999999
-        9999999999999999999999999999999999999999999999999999999999999999
-        9999999999999999999999999999999999999999999999999999999999999999
-        eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
-        eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
-        eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
-        eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
-        eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
-        `, SpriteKind.ground)
-    ground1.setPosition(scene.screenWidth() / 2, 100)
-}
-controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
-    animation.runImageAnimation(
-    ninja,
-    assets.animation`animRunUp`,
-    200,
-    false
-    )
-})
-controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
-    animation.runImageAnimation(
-    ninja,
-    assets.animation`animRunLeft`,
-    200,
-    false
-    )
-})
-controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
-    animation.runImageAnimation(
-    ninja,
-    assets.animation`animRunRight`,
-    200,
-    false
-    )
-})
-controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
-    animation.runImageAnimation(
+function AnimationMoving () {
+    characterAnimations.runFrames(
     ninja,
     assets.animation`animRunDown`,
     200,
-    false
+    characterAnimations.rule(Predicate.MovingDown)
     )
-})
-let ground1: Sprite = null
+    characterAnimations.runFrames(
+    ninja,
+    assets.animation`animRunUp`,
+    200,
+    characterAnimations.rule(Predicate.MovingUp)
+    )
+    characterAnimations.runFrames(
+    ninja,
+    assets.animation`animRunLeft`,
+    500,
+    characterAnimations.rule(Predicate.MovingLeft)
+    )
+    characterAnimations.runFrames(
+    ninja,
+    assets.animation`animRunRight`,
+    500,
+    characterAnimations.rule(Predicate.MovingRight)
+    )
+}
 let ninja: Sprite = null
 scene.setBackgroundColor(6)
 tiles.setCurrentTilemap(tilemap`map-demo`)
 ninja = sprites.create(assets.image`myImage1`, SpriteKind.Player)
 tiles.placeOnRandomTile(ninja, sprites.castle.rock2)
+AnimationMoving()
 scene.cameraFollowSprite(ninja)
 controller.moveSprite(ninja, 65, 65)
 ninja.setStayInScreen(true)
