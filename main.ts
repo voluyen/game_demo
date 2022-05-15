4c30cf91db696347b4b60dbdafbd475a40120512
voluyen <66213157+voluyen@users.noreply.github.com>
2022-05-15 23:44:23 +0700

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,84 @@
 namespace SpriteKind {
     export const ground = SpriteKind.create()
+    export const Weapon = SpriteKind.create()
 }
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (rightdir == true) {
+        projectile = sprites.createProjectileFromSprite(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . f . . . . . . . 
+            . . . . . . . f . . . . . . . . 
+            . . . . . . f . . . . . . . . . 
+            . . f . . . f f . . . . . . . . 
+            . . . f . f f f f f . . . . . . 
+            . . . . f f f f f . f . . . . . 
+            . . . . . . f f . . . f . . . . 
+            . . . . . . . f . . . . . . . . 
+            . . . . . . f . . . . . . . . . 
+            . . . . . f . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, ninja, 100, 0)
+    } else {
+        projectile2 = sprites.createProjectileFromSprite(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . f . . . . . . . 
+            . . . . . . . f . . . . . . . . 
+            . . . . . . f . . . . . . . . . 
+            . . f . . . f f . . . . . . . . 
+            . . . f . f f f f f . . . . . . 
+            . . . . f f f f f . f . . . . . 
+            . . . . . . f f . . . f . . . . 
+            . . . . . . . f . . . . . . . . 
+            . . . . . . f . . . . . . . . . 
+            . . . . . f . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, ninja, 0, -100)
+    }
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile5 = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . 1 c . . . 
+        . . . . . . . . . . . . c . . . 
+        . . . . . . . . . . . 1 c . . . 
+        . . . . . . . . . . . 1 c . . . 
+        . . . . . . . . . . . . c . . . 
+        . . . . . . . . . . . 1 c . . . 
+        . . . . . . . . . . . c . . . . 
+        . . . . . . . . . 1 1 c . . . . 
+        . . . . . . . . . . c . . . . . 
+        . . . . . . . . 1 c . . . . . . 
+        . . . . . . . . c 1 . . . . . . 
+        . . . . . . . 1 c . . . . . . . 
+        . 1 1 . 1 1 . c . . . . . . . . 
+        c c c c c c c 1 . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, ninja, 100, 0)
+    pause(100)
+    projectile5.destroy()
+    animation.runImageAnimation(
+    ninja,
+    assets.animation`myAnim8`,
+    50,
+    false
+    )
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    rightdir = false
+})
 statusbars.onZero(StatusBarKind.Health, function (status) {
     game.over(false)
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    rightdir = true
 })
 function AnimationMoving () {
     characterAnimations.runFrames(
@@ -20,20 +96,23 @@ function AnimationMoving () {
     characterAnimations.runFrames(
     ninja,
     assets.animation`animRunLeft`,
-    500,
+    200,
     characterAnimations.rule(Predicate.MovingLeft)
     )
     characterAnimations.runFrames(
     ninja,
     assets.animation`animRunRight`,
-    500,
+    200,
     characterAnimations.rule(Predicate.MovingRight)
     )
 }
-scene.onOverlapTile(SpriteKind.Player, sprites.builtin.crowd1, function (sprite, location) {
-    statusbar.value += -1
-    pause(200)
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.collectibleRedCrystal, function (sprite, location) {
+    statusbar.value += -0.15
 })
+let projectile5: Sprite = null
+let projectile2: Sprite = null
+let projectile: Sprite = null
+let rightdir = false
 let statusbar: StatusBarSprite = null
 let ninja: Sprite = null
 scene.setBackgroundColor(6)
@@ -46,5 +125,5 @@ controller.moveSprite(ninja, 65, 65)
 ninja.setStayInScreen(true)
 statusbar = statusbars.create(20, 4, StatusBarKind.Health)
 statusbar.setColor(7, 2)
-statusbar.max = 10
+statusbar.max = 20
 statusbar.attachToSprite(ninja, 0, 0)
--- tilemap.g.ts
+++ tilemap.g.ts
@@ -2,11 +2,13 @@
 namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
     export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
             case "map-demo":
-            case "level1":return tiles.createTilemap(hex`10001000090808080808080808080808080808080d0201010a020102010102020108070707010504030504040505030a0303090707020405101211050404030c03030a0707010405040303030404030c030a01070702040305030503030605040304010707020303040304050504030303050107070a030304050403030a03030305020f070a040513060405050303040404010e07010303030304050c0303050404010e07020b03060405030c0a03040503020f07020b03051012110a030504030301070701030306051012110305030303020707070203030504040505040b0b0a070707070701010a0201020101020107070708080808080808080808080808080808`, img`
+            case "level1":return tiles.createTilemap(hex`10001000060505050505050505050505050505050a02010107020102010102020105040404011111031111111111030703030604040211110d0f0e1111110309030307040401111111030303111103090307010404021103110311030311111103110104040203031103111111110303031101040407030311111103030703030311020c0407111110111111110303111111010b0401030303031111090303111111010b0402080311111103090703111103020c04020803110d0f0e07031111030301040401030311110d0f0e03110303030204040402030311111111101108080704040404040101070201020101020104040405050505050505050505050505050505`, img`
 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
 . . . . 2 2 2 2 2 2 2 2 2 2 2 2 
 2 . . . 2 . . . . . 2 2 2 2 2 2 
@@ -23,7 +25,7 @@ namespace myTiles {
 2 2 2 2 2 . . . . . . . . 2 2 2 
 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-`, [myTiles.transparency16,sprites.castle.tileGrass2,sprites.castle.tileGrass1,sprites.builtin.forestTiles0,sprites.castle.tileDarkGrass3,sprites.castle.tileDarkGrass2,sprites.castle.tileDarkGrass1,sprites.swamp.swampTile1,sprites.swamp.swampTile0,sprites.swamp.swampTile2,sprites.castle.saplingPine,sprites.castle.shrub,sprites.castle.rock0,sprites.castle.rock2,sprites.dungeon.stairLarge,sprites.dungeon.purpleOuterWest2,sprites.builtin.forestTiles1,sprites.builtin.forestTiles3,sprites.builtin.forestTiles2,sprites.builtin.crowd1], TileScale.Sixteen);
+`, [myTiles.transparency16,sprites.castle.tileGrass2,sprites.castle.tileGrass1,sprites.builtin.forestTiles0,sprites.swamp.swampTile1,sprites.swamp.swampTile0,sprites.swamp.swampTile2,sprites.castle.saplingPine,sprites.castle.shrub,sprites.castle.rock0,sprites.castle.rock2,sprites.dungeon.stairLarge,sprites.dungeon.purpleOuterWest2,sprites.builtin.forestTiles1,sprites.builtin.forestTiles3,sprites.builtin.forestTiles2,sprites.dungeon.collectibleRedCrystal,myTiles.tile1], TileScale.Sixteen);
         }
         return null;
     })
@@ -31,6 +33,8 @@ namespace myTiles {
     helpers._registerFactory("tile", function(name: string) {
         switch(helpers.stringTrim(name)) {
             case "transparency16":return transparency16;
+            case "myTile":
+            case "tile1":return tile1;
         }
         return null;
     })
